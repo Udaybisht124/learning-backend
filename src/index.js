98a78fb129427cd8c85817d51cfcd8297adc3ff5
udaybisht124 <bishtuday4668@gmail.coom>
2024-09-06 13:44:21 +0530

    database connection error here

---
+++ src/index.js
@@ -0,0 +1,132 @@
+// require('dotenv').config({path: './env'})
+import dotenv from 'dotenv';
+import mongoose from "mongoose";
+
+import connectDB from "./db/index.js";
+dotenv.config({
+    path:'./.env'
+})
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import dotenv from "dotenv";
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants.js";
+// import express from "express";
+// dotenv.config({
+//     path: './.env'
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const app = express();
+// ( async () => {
+//     try {
+//        const MongoDatabaseInstance =  await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+  
+//        app.on("errror", (error) => {
+//             console.log("ERRR: ", error);
+//             throw error
+//         })
+
+//         app.listen(process.env.PORT, () => {
+//             console.log(`App is listening on port ${process.env.PORT}`);
+//         })
+
+//     } catch (error) {
+//         console.error("ERROR: ", error)
+       
+//     }
+// })()
